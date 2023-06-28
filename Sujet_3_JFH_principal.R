3f437c9d8f7bde7b5eacee7128cccdc0c89d95d6
yisxpi <jean-francois.herve@insee.fr>
2023-06-28 13:51:22 +0000

    2023 06 28 15h45

--- Sujet_3_JFH_principal.R
+++ Sujet_3_JFH_principal.R
@@ -54,7 +54,6 @@ ponderations <- description_indiv %>%
 act_phys <- actphys_sedent %>% 
   left_join (ponderations,by="NOIND") %>% 
   mutate_at(c('pond_indiv_adu_pop2','pond_indiv_enf_pop1'), ~replace_na(.,0)) %>%
-  replace(is.na(.), 0) %>% 
   mutate(pond=pond_indiv_adu_pop2+pond_indiv_enf_pop1,
          act_jardin=activite_jardiner_score+activite_tondre_score+activite_arroser_score+activite_becher_score) %>% 
   mutate(categorie_agglo = case_when(agglo_5cl==1 ~ "a-Rural",
@@ -168,3 +167,53 @@ matrice_correlation2 <- model.matrix(~0+., data=df_num2) %>%
   cor(use="pairwise.complete.obs")
 matrice_correlation2 %>%
   ggcorrplot(show.diag=FALSE, type="lower", lab=TRUE, lab_size=7)
+
+#cartographie
+url <- "https://minio.lab.sspcloud.fr/projet-cartiflette/diffusion/shapefiles-test1/year%3D2022/administrative_level%3DREGION/crs%3D4326/FRANCE_ENTIERE%3Dmetropole/vectorfile_format%3D%27geojson%27/provider%3D%27IGN%27/source%3D%27EXPRESS-COG-CARTO-TERRITOIRE%27/raw.geojson"
+
+region <- sf::st_read(url)
+
+# Passons le fonds de carte dans le système de coordonnées de référence utilisé pour la FRance, Lambert 93 (code : 2154) au lieu de WGS 84
+region <- region %>% st_transform(2154)
+
+# Représentons les contours de notre fond de carte
+plot(st_geometry(region))
+
+region$NOM_M
+region <- region %>% mutate(NOM_M=ifelse(NOM_M=="CORSE", "PROVENCE-ALPES-COTE D'AZUR", NOM_M))
+
+view(act_phys$region_adm_12cl)
+
+act_phys3 <- act_phys %>% 
+  select(RUC_4cl, tage_PS, categorie_agglo, act_jardin,
+         activite_jardiner_score,activite_tondre_score,
+         activite_arroser_score,activite_becher_score,
+         region_adm_12cl) %>% 
+  mutate(region_recode=case_when(region_adm_12cl==1 ~ "ILE-DE-FRANCE",
+                                 region_adm_12cl==2 ~ "NORMANDIE",
+                                 region_adm_12cl==3 ~ "CENTRE-VAL DE LOIRE",
+                                 region_adm_12cl==4 ~ "PAYS DE LA LOIRE",
+                                 region_adm_12cl==5 ~ "BRETAGNE",
+                                 region_adm_12cl==6 ~ "HAUTS-DE-FRANCE",
+                                 region_adm_12cl==7 ~ "GRAND EST",
+                                 region_adm_12cl==8 ~ "BOURGOGNE-FRANCHE-COMTE",
+                                 region_adm_12cl==9 ~ "AUVERGNE-RHONE-ALPES",
+                                 region_adm_12cl==10 ~ "PROVENCE-ALPES-COTE D'AZUR",
+                                 region_adm_12cl==11 ~ "OCCITANIE",
+                                 region_adm_12cl==12 ~ "NOUVELLE-AQUITAINE"))
+
+actjardin_par_region <- act_phys3 %>% 
+  group_by(region_recode) %>% 
+  summarise(act_jardin=mean(act_jardin,na.rm=TRUE))
+actjardin_par_region
+
+# On crée un petit tableau avec nos régions et leurs attributs géographiques, 
+# et surtout la variable qu'on vient de calculer (c'est-à-dire le nombre de bières consommées par mois par région en moyenne)
+
+region_inca <- left_join(region,actjardin_par_region,by=c("NOM_M"="region_recode"))
+
+ggplot(data=region_inca) +
+  geom_sf(aes(fill=actjardin_par_region)) +
+  scale_fill_continuous(low="yellow", high="Red", name="activité jardin en moyenne en MET h/j") +
+  labs(title="intensité du jardinage en moyenne par région")
+
